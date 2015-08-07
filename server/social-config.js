f22d76bc25e529002da983cbc2d380ac67773986
Luping Lin <luping@igdra.com>
2015-08-06 22:58:27 -0700

    Rasic Facebook login using a test igdra app. Only tested locally. Need to switch to a real app.

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '932624673469077',
+    secret: '5e42bb772740b8574ef170996f47785a'
+});
