88ced0d97fff31507541f9de32e867edbfd6032d
Luping Lin <luping@igdra.com>
2015-08-07 12:01:22 -0700

    use offical igdra app for FB login.

--- server/social-config.js
+++ server/social-config.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '932624673469077',
-    secret: '5e42bb772740b8574ef170996f47785a'
+    appId: '961589217231396', //'932624673469077',
+    secret: '285b83bbde745ce8be4d22832dead779', // '5e42bb772740b8574ef170996f47785a'
 });
